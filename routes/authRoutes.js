a3e9369029931a9fe1290a95df74f5228296b2be
Nijat10 <nicatnasirov17@gmail.com>
2024-06-25 08:25:10 +0400

    Added: Express validator has been included for auth route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const { body } = require("express-validator");
 
-router.post("/registration", authController.registration);
-router.post("/login", authController.login);
+router.post(
+  "/registration",
+  [
+    body("fullname").not().isEmpty().withMessage("Fullname is required"),
+    body("email").not().isEmpty().withMessage("Username is required"),
+    body("password").not().isEmpty().withMessage("Password is required"),
+  ],
+  authController.registration
+);
+router.post(
+  "/login",
+  [body("email").not().isEmpty(), body("password").isLength({ min: 5 })],
+  authController.login
+);
 
 module.exports = router;
